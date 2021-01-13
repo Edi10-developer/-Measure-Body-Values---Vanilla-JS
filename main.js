4aa9765e4180527268a15d879b0b1201960c1d93
Edi <selimi.edi.1991@gmail.com>
2021-01-13 15:21:42 +0100

    Hppiness and Zodiac Sign
    Added 2 new functions to calculate your Zosiac Sign and how much happy your life is.

--- main.js
+++ main.js
@@ -203,7 +203,7 @@ function waterBalance() {
   let wBB = (weight / 100) * 60;
 
   result.innerHTML = wBB + "L";
-  
+
   body.push(wBB);
   fillBodyBoneMassData();
 }
@@ -224,7 +224,7 @@ function fillWaterBodyBalanceData() {
 function bodyBoneMass() {
   let weight = body[0];
   let result = document.getElementById("resultBone");
-  let bBM = Math.round(((weight / 100) * 3) * 100) / 100;
+  let bBM = Math.round((weight / 100) * 3 * 100) / 100;
 
   result.innerHTML = bBM + "Kg";
 
@@ -235,10 +235,112 @@ function fillBodyBoneMassData() {
   let weight = body[0];
   let bodyBoneData = document.getElementById("boneDiv");
 
- bodyBoneData.innerHTML = `
+  bodyBoneData.innerHTML = `
         <p>
         <label>Weight</label><br/>
         <input type="text" placeholder="${weight}" disabled/>
         </p>
         `;
+}
+
+// Zodiac Sign
+function zodiacSign() {
+  let month = document.getElementById("month").value;
+  let day = document.getElementById("day").value;
+  let result = document.getElementById("resultSign");
+  let sign = calculateZodiacSign(month, day);
+
+  result.innerHTML = sign;
+  body.push(sign);
+}
+
+function calculateZodiacSign(month, day) {
+  if ((day <= 19 && month == 1) || (day >= 22 && month == 12)) {
+    sign = "Capricorn";
+  } else if ((day > 19 && month == 1) || (day <= 19 && month == 2)) {
+    sign = "Aquarium";
+  } else if ((day >= 20 && month == 2) || (day <= 20 && month == 3)) {
+    sign = "Pez";
+  } else if ((day > 20 && month == 3) || (day <= 20 && month == 4)) {
+    sign = "Aries";
+  } else if ((day > 21 && month == 4) || (day <= 20 && month == 5)) {
+    sign = "Taurus";
+  } else if ((day > 20 && month == 5) || (day <= 21 && month == 6)) {
+    sign = "Gemini";
+  } else if ((day > 21 && month == 6) || (day <= 22 && month == 7)) {
+    sign = "Cancer";
+  } else if ((day > 22 && month == 7) || (day <= 22 && month == 8)) {
+    sign = "Leo";
+  } else if ((day > 22 && month == 8) || (day <= 23 && month == 9)) {
+    sign = "Virgo";
+  } else if ((day > 23 && month == 9) || (day <= 22 && month == 10)) {
+    sign = "Libra";
+  } else if ((day > 22 && month == 10) || (day <= 22 && month == 11)) {
+    sign = "Escorpio";
+  } else if ((day > 22 && month == 11) || (day <= 21 && month == 12)) {
+    zodiacSimbol = "Sagittarius";
+  } else {
+    alert("Please fill the form with correct information");
+  }
+  return sign;
+}
+
+// Happiness
+function happiness() {
+  let happyVal = parseInt(document.getElementById("happyVal").value);
+  let happyRes;
+  let result = document.getElementById("resultHappiness");
+
+  switch (happyVal) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+      happyRes = "You don't like your life, something must to change.";
+      break;
+    case 5:
+      happyRes = "You are dissatisfied but with a little effort you can improve your well-being.";
+      break;
+    case 6:
+    case 7:
+      happyRes = "Are you basically satisfied with your life.";
+      break;
+    case 8:
+      happyRes = "You are a happy person.";
+      break;
+    case 9:
+      happyRes = "You are a happy and satisfied person, life suits you very well.";
+      break;
+    case 10: 
+    happyRes = "You are a happy and satisfied person, life could not have been better for you. Keep it up!"
+      break;
+    default: 
+    alert("Please fill the form with correct information");
+        }
+
+    result.innerHTML = happyRes;
+    body.push(happyRes);
+}
+
+// Body Status Value
+function getBodyValue() {
+  let result = document.getElementById("resultBodyStatus");
+
+  result.innerHTML = `
+  <div id="bodyStatusResult">
+  <p>Your sex is: ${body[4]}</p>
+  <p>Your age is: ${body[3]}</p>
+  <p>Your zodiac sign is: ${body[13]}</p>
+  <p>Your weight is: ${body[0]} Kg</p>
+  <p>Your height is: ${body[1]} cm</p>
+  <p>Your body mass is: ${body[2]}</p>
+  <p>Your basal metabolism is: ${body[5]} KCal</p>
+  <p>Your daily caloric need is: ${body[7]} Kcal</p>
+  <p>Your lean body mass is: ${body[8]} Kg</p>
+  <p>Your fat body mass is: ${body[9]} %. ${body[10]}</p>
+  <p>Your body water balance is: ${body[11]} l</p>
+  <p>Your body bone mass is: ${body[12]} Kg</p>
+  <p>The intensity of your lifestyle activity is: ${body[6]}</p>
+  <p>Your happiness is: ${body[6]}</p>
+  </div>`;
 }
